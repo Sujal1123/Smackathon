d81287f488be7376b9a38c27b9792018b4bc23e6
Sujal Dandhale <sujaldandhale94@gmail.com>
2024-08-23 22:04:18 +0530

    New Changes.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const passport = require("passport");
+const localStorge = require("passport-local");
+const User = require("./models/user.js");
+const session = require("express-session");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/smackathon";
 
@@ -29,14 +33,35 @@ app.get("/", (req,res) => {
     res.render("../views/listings/home.ejs");
 });
 
+app.use(
+    session({
+        secret: "mysecretcode",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use(passport.initialize());
+
 app.get("/trackingexpense", (req,res) => {
     res.render("../views/listings/tracking.ejs");
 });
 
+app.get("/managingdebt", (req,res) => {
+    res.render("../views/listings/managing.ejs");
+});
+
+app.get("/financialgoal", (req,res) => {
+    res.render("../views/listings/financialgoal.ejs");
+});
 
 app.get("/contact", (req,res) => {
     res.render("../views/listings/contact.ejs");
 });
+
+app.get("/login", (req,res) => {
+    res.render("")
+})
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
