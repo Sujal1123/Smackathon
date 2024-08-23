b74bf84fbac185075e970b204f0f10992dee6ca8
Sujal Dandhale <sujaldandhale94@gmail.com>
2024-08-23 18:56:43 +0530

    Making basic environment.

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/smackathon";
+
+main().then(() => {
+    console.log("connected to db");
+}).catch(err => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("views engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/", (req,res) => {
+   res.send("this is your home page");
+});
+
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
